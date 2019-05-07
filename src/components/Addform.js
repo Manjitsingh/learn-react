2cecb30b447c6e5355ee3c730a0c4ef745e71962
Manjit <manjit@Manjits-MacBook-Pro.local>
2019-05-07 09:49:09 +0530

    Add form Component added

---
+++ src/components/Addform.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Addform extends React.Component {
+    constructor(props) {
+        super(props);
+        // Next we establish our state
+        this.state = {
+            text: '',
+        }
+    }
+    render() {
+        return(
+                <div className="csdf">
+                    <p>{ this.props.text }</p>
+                </div>
+        )
+    }
+
+}
+
+export default Addform;
