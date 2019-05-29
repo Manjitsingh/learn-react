4a1e27c4c1dbf9631f95b84fbd890c16aca40012
Manjit Singh <manjit.singh@zscaler.com>
2019-05-29 10:59:09 +0530

    Object created for Add form

--- src/components/Addform.js
+++ src/components/Addform.js
@@ -1,40 +1,70 @@
 import React from 'react';
 
 class Addform extends React.Component {
-    state = {
-        firstName: "",
-        lastName: "",
-        userName: "",
-        email: "",
-        password: ""
-    };
 
-    change = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Add Form',
+            datas: [],
+        };
+    }
 
-    onSubmit = () => {
+    componentDidMount() {
+        this.refs.firstName.focus();
+    }
+
+    // change = (e) => {
+    //     this.setState({
+    //         [e.target.name]: e.target.value
+    //     });
+    // };
+
+    onSubmit = (e) => {
+        e.preventDefault();
         console.log(this.state);
-        debugger;
+
+        let datas = this.state.datas;
+        let firstName = this.refs.firstName.value;
+        let lastName = this.refs.lastName.value;
+        let userName = this.refs.userName.value;
+        let email = this.refs.email.value;
+        let password = this.refs.password.value;
+        let data = {
+            firstName, lastName, password, userName, email
+        };
+        datas.push(data);
+
+        this.setState({
+            datas: datas
+        });
+
+        this.refs.addForm.reset();
+        this.refs.firstName.focus();
     };
 
     render() {
+        let datas = this.state.datas;
         return(
                 <div className="add-form">
-                    <form>
-                        <input type="text" name="firstName" placeholder="First Name" value={this.state.firstName} onChange={ e => this.change(e) } /><br />
-                        <input type="text" name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={ e => this.change(e) } /><br />
-                        <input type="text" name="userName" placeholder="Username" value={this.state.userName} onChange={ e => this.change(e) } /><br />
-                        <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={ e => this.change(e) } /><br />
-                        <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={ e => this.change(e) } /><br />
-                        <button onClick={ () => this.onSubmit() }>Submit</button><br />
+                    <form ref="addForm">
+                        <input type="text" ref="firstName" placeholder="First Name" value={this.state.firstName} /><br />
+                        <input type="text" ref="lastName" placeholder="Last Name" value={this.state.lastName} /><br />
+                        <input type="text" ref="userName" placeholder="Username" value={this.state.userName} /><br />
+                        <input type="email" ref="email" placeholder="Email" value={this.state.email} /><br />
+                        <input type="password" ref="password" placeholder="Password" value={this.state.password} /><br />
+                        <button onClick={ (e) => this.onSubmit(e) }>Submit</button><br />
                     </form>
+                    <pre>
+                        { datas.map((data, i) =>
+                            <li key={i} className="submittedValues">
+                                {i+1} . {data.firstName}, {data.lastName},{data.userName},{data.email},{data.password},
+                            </li>
+                        )}
+                    </pre>
                 </div>
         )
     }
-
 }
 
 export default Addform;
